16fad2832ddba3e6536fefdcbb252c0ba0e7abed
Joe Mou <dev@mou.fo>
2025-05-19 21:10:08 -0400

    Place multi events without overlap
    Rather than rendering multi events individually, we decompose them into
    edges. These are sorted into a processing order, and we allocate
    placement using a greedy strategy.
    
    Switch most of our logic to exclusive end dates, since this gives a more
    natural ordering. We continue to use inclusive end dates for some
    portions of grid and text formatting.

--- web/main.ts
+++ web/main.ts
@@ -48,16 +48,20 @@ function escapeHtml(s: string) {
 abstract class Event {
   constructor(protected readonly data: EventData) {}
 
-  abstract get multi(): boolean;
-
   abstract get startDate(): Temporal.PlainDate;
   abstract get endDate(): Temporal.PlainDate;
 
-  get gridStart() {
-    return this.startDate;
-  }
+  // If the event should span multiple grid cells, return the number of cells to
+  // span. Otherwise for an event within a single day, return 0.
+  abstract multiLength(): number;
 
-  abstract get gridEnd(): Temporal.PlainDate;
+  compare(other: Event) {
+    let rc = this.data.startAt.localeCompare(other.data.startAt);
+    if (rc != 0) return rc;
+    rc = -this.data.endAt.localeCompare(other.data.endAt);
+    if (rc != 0) return rc;
+    return this.data.title.localeCompare(other.data.title);
+  }
 
   protected abstract formatTime(): string;
   abstract renderHeadline(): string;
@@ -70,33 +74,29 @@ abstract class Event {
 }
 
 class DateEvent extends Event {
-  get multi() {
-    return true;
-  }
-
   get startDate() {
     return Temporal.PlainDate.from(this.data.startAt);
   }
 
   get endDate() {
-    // Make end date inclusive.
-    return Temporal.PlainDate.from(this.data.endAt).subtract({ days: 1 });
+    return Temporal.PlainDate.from(this.data.endAt);
   }
 
-  get gridEnd() {
-    return this.endDate;
+  multiLength() {
+    return this.startDate.until(this.endDate).days;
   }
 
   protected formatTime() {
-    if (this.startDate.equals(this.endDate)) {
+    const inclusiveEnd = this.endDate.subtract({ days: 1 });
+    if (this.startDate.equals(inclusiveEnd)) {
       return formatMonDate(this.startDate);
     } else if (
-      this.startDate.with({ day: this.endDate.day }).equals(this.endDate)
+      this.startDate.with({ day: inclusiveEnd.day }).equals(inclusiveEnd)
     ) {
-      return `${formatMonDate(this.startDate)}&ndash;${this.endDate.day}`;
+      return `${formatMonDate(this.startDate)}&ndash;${inclusiveEnd.day}`;
     } else {
       return `${formatMonDate(this.startDate)}&ndash;${
-        formatMonDate(this.endDate)
+        formatMonDate(inclusiveEnd)
       }`;
     }
   }
@@ -107,10 +107,6 @@ class DateEvent extends Event {
 }
 
 class DateTimeEvent extends Event {
-  get multi() {
-    return !this.gridStart.equals(this.gridEnd);
-  }
-
   private get startAt() {
     return localDateTime(this.data.startAt);
   }
@@ -127,15 +123,11 @@ class DateTimeEvent extends Event {
     return this.endAt.toPlainDate();
   }
 
-  get gridEnd() {
-    return this.startDate.equals(this.endDate)
-      ? this.endDate
-      // Google Calendar includes the end day if past noon.
-      : this.endAt.subtract({ hours: 12 }).toPlainDate();
-  }
-
-  compare(other: DateTimeEvent) {
-    return Temporal.ZonedDateTime.compare(this.startAt, other.startAt);
+  multiLength() {
+    if (this.startDate.equals(this.endDate)) return 0;
+    // Google Calendar includes the end day if past noon.
+    return this.endAt.add({ hours: 12 }).toPlainDate()
+      .since(this.startDate).days;
   }
 
   private get briefStartTime() {
@@ -160,107 +152,193 @@ class DateTimeEvent extends Event {
   }
 }
 
+interface MultiEventEdge {
+  gridOrd: number;
+  event: Event;
+  prev: MultiEventEdge | null;
+  definite: boolean;
+  place?: number;
+}
+
 export class CalendarRenderer {
   private calendarStart: Temporal.PlainDate;
   private calendarDays: number;
 
-  constructor(year: number, private data: CalendarData) {
+  constructor(year: number) {
     const jan1 = new Temporal.PlainDate(year, 1, 1);
     this.calendarStart = jan1.subtract({ days: jan1.dayOfWeek });
+
     const dec31 = new Temporal.PlainDate(year, 12, 31);
-    const limit = dec31.add({ days: 6 - dec31.dayOfWeek });
-    this.calendarDays = this.calendarStart.until(limit).days;
+    const inclusiveEnd = dec31.add({ days: 6 - dec31.dayOfWeek });
+    this.calendarDays = this.calendarStart.until(inclusiveEnd).days + 1;
   }
 
-  private gridCoords(date: Temporal.PlainDate) {
-    const index = date.since(this.calendarStart).days;
-    const row = Math.floor(index / 7) + 1;
-    const col = index % 7 + 1;
-    return { row, col };
+  private gridOrd(date: Temporal.PlainDate) {
+    return date.since(this.calendarStart).days;
+  }
+
+  private gridRow(ord: number) {
+    return Math.floor(ord / 7) + 1;
+  }
+
+  private gridCol(ord: number) {
+    return ord % 7 + 1;
   }
 
   private renderDay(
-    day: Temporal.PlainDate,
+    ord: number,
     stack: Event[],
-    includeMonth: boolean,
+    ensureMonth: boolean,
+    space: number,
   ) {
-    const text = includeMonth ? formatMonDate(day) : day.day.toString();
-    const events = [];
-    while (stack.at(-1)?.startDate?.equals(day)) {
+    const date = this.calendarStart.add({ days: ord });
+    ensureMonth ||= date.day === 1;
+    const text = ensureMonth ? formatMonDate(date) : date.day.toString();
+
+    const content = [];
+    if (space > 0) {
+      content.push(`<div class=space data-num-places="${space}"></div>`);
+    }
+    while (stack.at(-1)?.startDate?.equals(date)) {
       const event = stack.pop() as Event;
-      events.push(`
+      content.push(`
         <div class=event title="${event.renderTitleAttr()}">
           ${event.renderHeadline()}
         </div>
       `);
     }
-    const { row, col } = this.gridCoords(day);
+
+    const row = this.gridRow(ord);
+    const col = this.gridCol(ord);
     return `
       <div class=day style="grid-area: ${row} / ${col}">
-        <time datetime=${day}>${text}</time>
-        ${events.join("")}
+        <time datetime=${date}>${text}</time>
+        ${content.join("")}
       </div>
     `;
   }
 
-  private renderMultiDayEvent(event: Event) {
-    const start = this.gridCoords(event.gridStart);
-    const end = this.gridCoords(event.gridEnd);
-    const result = [];
-    for (let row = start.row; row <= end.row; row++) {
-      const left = row == start.row ? start.col : 1;
-      const right = row == end.row ? end.col + 1 : -1;
-      result.push(`
-        <div class="event multi" style="--index: 0"
-             data-area="${row} / ${left} / ${row} / ${right}"
-             title="${event.renderTitleAttr()}">
-          ${event.renderHeadline()}
-        </div>
-      `);
+  private decomposeMultiEdges(events: Event[]): MultiEventEdge[] {
+    const edges = [];
+    for (const event of events) {
+      // Leading edge.
+      const start = this.gridOrd(event.startDate);
+      let prev = { gridOrd: start, event, prev: null, definite: true };
+      edges.push(prev);
+
+      // Add indefinite edges (week boundaries) starting from Sunday.
+      const followingSunday = start - (start % 7) + 7;
+      const end = start + event.multiLength();
+      for (let gridOrd = followingSunday; gridOrd < end; gridOrd += 7) {
+        edges.push({ gridOrd, event, prev, definite: false });
+        prev = { gridOrd, event, prev: null, definite: false };
+        edges.push(prev);
+      }
+
+      // Final trailing edge.
+      edges.push({ gridOrd: end, event, prev, definite: true });
     }
-    return result.join("");
+    return edges;
   }
 
-  private renderVariables() {
+  private compareMultiEdge(a: MultiEventEdge, b: MultiEventEdge) {
+    // Date ascending.
+    if (a.gridOrd != b.gridOrd) return a.gridOrd < b.gridOrd ? -1 : 1;
+    // Close then open.
+    if (a.prev != b.prev) return a.prev != null ? -1 : 1;
+    // Indefinite then definite.
+    if (a.definite != b.definite) return !a.definite ? -1 : 1;
+    // Compare the rest of the event, mostly to keep sorting more stable.
+    return a.event.compare(b.event);
+  }
+
+  private placeMultiEdges(edges: MultiEventEdge[]) {
+    const allocatedSpace = [];
+    const places = [];
+    for (const edge of edges) {
+      const { gridOrd, prev } = edge;
+      if (prev == null) {
+        let place = 0;
+        while (place < places.length && places[place] != null) place++;
+        places[place] = true;
+        edge.place = place;
+        allocatedSpace[gridOrd] = places.length;
+      } else if (prev.place != null) {
+        console.assert(places[prev.place]);
+        delete places[prev.place];
+        while (places.length > 0 && places.at(-1) == null) places.pop();
+        allocatedSpace[gridOrd] = places.length;
+      }
+    }
+    return allocatedSpace;
+  }
+
+  private renderMultiEdge(edge: MultiEventEdge) {
+    if (edge.prev == null) return "";
+    const { event, prev } = edge;
+    console.assert(prev.place != null);
+
+    const row = this.gridRow(prev.gridOrd);
+    const left = this.gridCol(prev.gridOrd);
+    const inclusiveEnd = edge.gridOrd - 1;
+    console.assert(row === this.gridRow(inclusiveEnd));
+    const right = this.gridCol(inclusiveEnd) + 1;
+    // TODO style indefinite edges
+    return `
+      <div class="event multi" data-place="${prev.place}"
+           data-grid-area="${row} / ${left} / ${row} / ${right}"
+           title="${event.renderTitleAttr()}">
+        ${event.renderHeadline()}
+      </div>
+    `;
+  }
+
+  private renderVariables(data: CalendarData) {
     const normal: DateTimeEvent[] = [];
     const multi: Event[] = [];
-    for (const eventData of this.data.events) {
+    for (const eventData of data.events) {
       const hasTime = eventData.startAt.includes("T");
       if (eventData.endAt.includes("T") != hasTime) {
         throw new Error("Date/DateTime mismatch");
       }
       if (hasTime) {
         const event = new DateTimeEvent(eventData);
-        const queue = event.multi ? multi : normal;
+        const queue = event.multiLength() > 0 ? multi : normal;
         queue.push(event);
       } else {
         multi.push(new DateEvent(eventData));
       }
     }
-    // Soonest events at top of stack.
-    normal.sort((a, b) => b.compare(a));
 
+    // Place multi events first, so we know how much space to allocate for them
+    // on the grid.
+    const edges = this.decomposeMultiEdges(multi);
+    edges.sort(this.compareMultiEdge);
+    const allocatedSpace = this.placeMultiEdges(edges);
+
+    // Render the calendar grid and normal events.
     const content = [];
-
+    normal.sort((a, b) => b.compare(a)); // earliest events at top of stack
+    let space = 0;
     for (let i = 0; i < this.calendarDays; i++) {
-      const day = this.calendarStart.add({ days: i });
-      const includeMonth = day.day === 1 || day.equals(this.calendarStart);
-      content.push(this.renderDay(day, normal, includeMonth));
+      if (allocatedSpace[i] != null) space = allocatedSpace[i];
+      content.push(this.renderDay(i, normal, i === 0, space));
     }
 
-    for (const event of multi) {
-      content.push(this.renderMultiDayEvent(event));
+    // Render multi events over the calendar.
+    for (const edge of edges) {
+      content.push(this.renderMultiEdge(edge));
     }
 
     return {
-      name: this.data.name,
+      name: data.name,
       content: content.join(""),
     };
   }
 
-  async render() {
+  async render(data: CalendarData) {
     const template = await Deno.readTextFile("template.html");
-    const { name, content } = this.renderVariables();
+    const { name, content } = this.renderVariables(data);
     return template
       .replace("{{ name }}", name)
       .replace("{{ content }}", content);
@@ -274,7 +352,7 @@ if (import.meta.main) {
   }
 
   const data = JSON.parse(Deno.readTextFileSync(Deno.args[0]));
-  const renderer = new CalendarRenderer(2025, data);
+  const renderer = new CalendarRenderer(2025);
 
-  console.log(await renderer.render());
+  console.log(await renderer.render(data));
 }
--- web/main_test.ts
+++ web/main_test.ts
@@ -6,6 +6,6 @@ Deno.test("isSnapshotMatch", async (t) => {
   const data = JSON.parse(
     Deno.readTextFileSync("__snapshots__/inputs/asp-fgpc.json"),
   );
-  const renderer = new CalendarRenderer(2025, data);
-  await assertSnapshot(t, await renderer.render());
+  const renderer = new CalendarRenderer(2025);
+  await assertSnapshot(t, await renderer.render(data));
 });
